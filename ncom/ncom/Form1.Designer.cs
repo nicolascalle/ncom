5a6fb0f40e72c55dd75218e0b900dbd7b7388a75
nicolascalle <callenicolasg@gmail.com>
2019-04-05 16:11:24 -0300

    Primer commit

--- ncom/ncom/Form1.Designer.cs
+++ ncom/ncom/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ncom
 {
-    partial class Form1
+    partial class inicio
     {
         /// <summary>
         /// Variable del diseñador requerida.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "inicio";
+            this.Text = "Inicio";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
