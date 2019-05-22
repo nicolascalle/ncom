c11a9684e325634994a0a4198aa797a7cb36b554
agustinemartinez <agus97martinez@gmail.com>
2019-05-22 18:31:14 -0300

    Agrego la frecuencia para la creacion de fasores y pongo el enum fuera de la clase

--- ncom/ncom/model/Fasor.cs
+++ ncom/ncom/model/Fasor.cs
@@ -4,20 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ncom.model
-{
-    class Fasor{
+namespace ncom.model {
+
+    enum Funcion { COS, SEN }
+
+    class Fasor {
         private double amplitud;
         private Funcion funcion;
+        private double frecuencia;
         private double fase;
-
-        enum Funcion{
-            COSENO,
-            SENO
-        }
         
-        private Funcion COSENO { get; set; }
-        private Funcion SENO { get; set; }
         public double GetAmplitud() { return amplitud; }
         public Funcion GetFuncion() { return funcion; }
         public double GetFase() { return fase; }
@@ -26,27 +22,33 @@ namespace ncom.model
         public void SetFuncion(Funcion funcion) { this.funcion = funcion; }
         public void SetFase(double fase) { this.fase = fase; }
 
-        public Fasor(double amplitud, Funcion funcion, double fase){
+        public Fasor(double amplitud, Funcion funcion, double frecuencia, double fase) {
             this.amplitud = amplitud;
             this.funcion = funcion;
             this.fase = fase;
         }
 
-        private Fasor sumarFasores(Fasor fasor){
-            if(this.funcion == SENO){
+        override
+        public String ToString() {
+            return amplitud + " . " + funcion.ToString().ToLower() + " ( " + frecuencia + ".t" + " + " + fase + " )";
+        }
+
+        public Fasor SumarFasor(Fasor fasor){
+            if(this.funcion == Funcion.SEN){
                 this.fase = this.fase - Math.PI / 2;
             }
-            if (fasor.funcion == SENO){
+            if (fasor.funcion == Funcion.SEN){
                 fasor.fase = fasor.fase - Math.PI / 2;
             }
 
             double parteReal = this.amplitud * Math.Cos(this.fase) + fasor.amplitud * Math.Cos(fasor.fase);
             double parteImaginaria = this.amplitud * Math.Sin(this.fase) + fasor.amplitud * Math.Sin(fasor.fase);
             ComplejoBinomica binomica = new ComplejoBinomica(parteReal, parteImaginaria);
-            double amplitud = binomica.CalcularModulo();
-            double fase = binomica.CalcularArgumento();
+            double amplitud = binomica.ToPolar().GetModulo();
+            double fase = binomica.ToPolar().GetArgumento();
+            double frecuencia = 0;
 
-            return new Fasor(amplitud, COSENO , fase);
+            return new Fasor(amplitud, Funcion.COS, frecuencia, fase);
         }       
     }
 }
